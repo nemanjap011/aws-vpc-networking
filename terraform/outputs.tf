2cd540f994603ef2c31b2523d34b97dd7d1338c3
Nem <155702438+nemanjap011@users.noreply.github.com>
2025-07-22 13:32:20 -0400

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,31 @@
+```hcl
+output "vpc_id" {
+  value       = aws_vpc.cloudops.id
+  description = "ID of the VPC"
+}
+
+output "public_subnet_id" {
+  value       = aws_subnet.public_a.id
+  description = "Public subnet ID"
+}
+
+output "private_subnet_id" {
+  value       = aws_subnet.private_a.id
+  description = "Private subnet ID"
+}
+
+output "bastion_public_ip" {
+  value       = aws_instance.bastion.public_ip
+  description = "Public IP of the jump host"
+}
+
+output "bastion_private_ip" {
+  value       = aws_instance.bastion.private_ip
+  description = "Private IP of the jump host"
+}
+
+output "app_private_ip" {
+  value       = aws_instance.app.private_ip
+  description = "Private IP of the app server"
+}
+```
